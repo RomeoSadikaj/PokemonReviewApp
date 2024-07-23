8160025b7db8c9a78d132e0ff1597d337102fc12
RomeoSadikaj <romeosadikaj@gmail.com>
2024-07-23 17:32:32 +0200

    Add project files.

---
+++ PokemonReviewApp/Interfaces/ICountryRepository.cs
@@ -0,0 +1,17 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetCountries();
+        Country GetCountry(int id);
+        Country GetCountryByOwner(int ownerId);
+        ICollection<Owner> GetOwnersFromACountry(int countryId);
+        bool CountryExists(int id);
+        bool UpdateCountry(Country country);
+        bool CreateCountry(Country country);
+        bool DeleteCountry(Country country);
+        bool Save();
+    }
+}
---
+++ PokemonReviewApp/Repository/ReviewerRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class ReviewerRepository : IReviewerRepository
+    {
+        private readonly DataContext _context;
+
+        public ReviewerRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool CreateReviewer(Reviewer reviewer)
+        {
+            _context.Add(reviewer);
+            return Save();
+        }
+
+        public bool DeleteReviewer(Reviewer reviewer)
+        {
+            _context.Remove(reviewer);
+            return Save();
+        }
+
+        public Reviewer GetReviewer(int reviewerId)
+        {
+            return _context.Reviewers.Where(r => r.Id == reviewerId).Include(e => e.Reviews).FirstOrDefault();
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            return _context.Reviewers.ToList(); 
+        }
+
+        public ICollection<Review> GetReviewsByReviewer(int reviewerId)
+        {
+            return _context.Reviews.Where(r => r.Reviewer.Id == reviewerId).ToList();
+        }
+
+        public bool ReviewerExists(int reviewId)
+        {
+            return _context.Reviewers.Any(r => r.Id == reviewId);
+        }
+
+        public bool Save()
+        {
+            var Saved = _context.SaveChanges();
+            return Saved > 0 ? true : false;
+        }
+
+        public bool UpdateReviewer(Reviewer reviewer)
+        {
+            _context.Update(reviewer);
+            return Save();
+        }
+    }
+}
